1bb5468ba31276f2d64d2b986a7ff6dcf33d4b91
Rayssa Vilaca <cecilia_vilaca@outlook.com>
2020-04-01 23:12:31 -0300

    Usando finally

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,29 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		method1();
-		System.out.println("End of program");
-
-	}
-	
-	public static void method1() {
-		System.out.println("***METHOD1 START***");
-		method2();
-		System.out.println("***METHOD1 END***");	
-	}
-
-	public static void method2() {
-		System.out.println("***METHOD2 START***");
-		Scanner sc = new Scanner(System.in);
+		
+		File file = new File("/home/rayssa/Área de Trabalho/aqui.txt");
+		Scanner sc = null;
+		
 		try {
-			String[] vect = sc.nextLine().split(" ");
-			int position = sc.nextInt();
-			System.out.println(vect[position]);
-
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Invalid position!");
-			e.printStackTrace();
-			sc.next();
-		} catch (InputMismatchException e) {
-			System.out.println("Input error");
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if(sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed!");
 		}
-
-		sc.close();
-		System.out.println("***METHOD2 END***");
+		
 	}
-
 }
